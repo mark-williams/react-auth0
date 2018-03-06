f352a7c7ecf9b01c9b0c7612ac8631da926cf9a7
mark-williams <markwill@hotmail.co.uk>
2018-03-06 10:56:36 +0000

    refactor: css animation for logo now implemented using styled-components

--- src/components/HeaderLogo.js
+++ src/components/HeaderLogo.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import logo from '../assets/logo.svg';
-import './App.css';
+import styled, { keyframes } from 'styled-components';
+
+const rotate360 =  keyframes`
+  from { transform: rotate(0deg); }
+  to { transform: rotate(360deg); }
+`;
+
+const SpinningLogo = styled.img`
+  animation: ${rotate360} infinite 20s linear;
+  height: 80px;
+`;
 
 const HeaderLogo = () => (
-  <img src={logo} className="App-logo" alt="logo" />
+  <SpinningLogo src={logo} className="App-logo" alt="logo" />
 );
 
 export default HeaderLogo;
