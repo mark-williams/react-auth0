7a778d894fd5ed306ce51e755f9dcbfb8f569340
mark-williams <markwill@hotmail.co.uk>
2018-03-07 12:23:13 +0000

    created tests for auth/not auth (separate mocks of authService).

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -2,13 +2,13 @@ import React from 'react';
 import Enzyme, { mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import App from './App';
+import authService from '../services/authService';
 
 Enzyme.configure({ adapter: new Adapter() });
 
-jest.mock('../services/authService', () => {
-  return jest.fn().mockImplementation(() => {
-    return { isAuthenticated: () => (false) };
-  });
+jest.mock('../services/authService');
+authService.mockImplementation(() => {
+  return { isAuthenticated: () => (false) };
 });
 
 describe('App component', () => {
@@ -17,10 +17,20 @@ describe('App component', () => {
     expect(wrapper).toBeDefined();
   });
 
-  it('renders login if user not authenticated', () => {
+  it('renders login button if user not authenticated', () => {
     const wrapper = mount(<App />);
     const loginButton = wrapper.find('button');
 
     expect(loginButton.length).toBe(1);
+  });
+
+  it('does not render login button if user is authenticated', () => {
+    authService.mockImplementation(() => {
+      return { isAuthenticated: () => (true) };
+    });
+    const wrapper = mount(<App />);
+    const loginButton = wrapper.find('button');
+
+    expect(loginButton.length).toBe(0);
   });
 });
